e96d98dcc65872ab2d36e04b0f828fbf2fd3b609
mohamedelbadawi <38269461+mohamedelbadawi@users.noreply.github.com>
2022-12-09 15:09:16 +0200

    Time: 5081 ms (5.42%), Space: 70 MB (66.19%) - LeetHub

--- 2001-number-of-pairs-of-interchangeable-rectangles/2001-number-of-pairs-of-interchangeable-rectangles.py
+++ 2001-number-of-pairs-of-interchangeable-rectangles/2001-number-of-pairs-of-interchangeable-rectangles.py
@@ -1,14 +1,12 @@
 class Solution:
     def interchangeableRectangles(self, rectangles: List[List[int]]) -> int:
-         mp=collections.defaultdict(list)
-         res=0
-         for i, [width, height] in enumerate(rectangles):
-                mp[float(width)/height].append(i)
-         for i in mp.keys():
-            l = len(mp[i])
-            res += l*(l-1)//2 # combinations = nc2
-         return res
-        
-            
-
-
+        mp={}
+        res=0
+        for i in rectangles:
+            if (i[0]/i[1]) in mp:
+               mp[(i[0]/i[1])]+=1
+            else:
+                mp[(i[0]/i[1])]=1
+        for i in (mp.values()):
+           res+=i*(i-1)//2
+        return res
