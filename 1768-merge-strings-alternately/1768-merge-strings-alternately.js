ec2639f4878a80dc029e23c08e9e3131ccd66118
mohamedelbadawi <38269461+mohamedelbadawi@users.noreply.github.com>
2023-05-19 10:04:55 +0300

    Time: 52 ms (88.14%), Space: 42.3 MB (57.87%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.js
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.js
@@ -3,23 +3,19 @@
  * @param {string} word2
  * @return {string}
  */
-var mergeAlternately = function (word1, word2) {
-    let merged = '';
-    let len = Math.min(word1.length, word2.length)
-    for (let i = 0; i < len; i++) {
-        merged += word1[i]
-        merged += word2[i]
-
+var mergeAlternately = function(word1, word2) {
+ let ans=''
+    for(let i=0;i<Math.min(word1.length,word2.length);i++){
+      ans+=word1[i]
+      ans+=word2[i]
+  }
+    
+    if(word1.length<word2.length){
+        ans+=word2.slice(word1.length,word2.length+1)
     }
-    word2 = word2.substring(len, word2.length)
-    word1 = word1.substring(len, word1.length)
-    if (word1) {
-        merged+=word1
+    
+    else if(word2.length<word1.length){
+        ans+=word1.slice(word2.length,word1.length+1)
     }
-    if (word2) {
-        merged+=word2
-    }
-
-
-    return merged;
+    return ans;
 };
